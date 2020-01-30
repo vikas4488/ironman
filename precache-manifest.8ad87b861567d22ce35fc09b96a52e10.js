13ef271a16e5fd4cceb28baffae6562e4576853f
vikas4488 <40414161+vikas4488@users.noreply.github.com>
2020-01-30 15:18:39 +0530

    Updates

---
+++ precache-manifest.8ad87b861567d22ce35fc09b96a52e10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7e942cf3c5c600af930bd7399f815d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6a241053b58a37a1cd3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b25df1467b56d63fc95f",
+    "url": "/static/js/2.e7c0d2b0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e7c0d2b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6a241053b58a37a1cd3",
+    "url": "/static/js/main.b5703816.chunk.js"
+  },
+  {
+    "revision": "796369d331dbd56391d8",
+    "url": "/static/js/runtime-main.3573e7ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
